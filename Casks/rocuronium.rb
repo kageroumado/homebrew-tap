e820493f003c9ed1585675986c289702a297a08f
kageroumado <mail@kagerou.glass>
2026-09-19 23:53:59 +0200

    Bump rocuronium to 1.2

--- Casks/rocuronium.rb
+++ Casks/rocuronium.rb
@@ -1,6 +1,6 @@
 cask "rocuronium" do
-  version "1.1"
-  sha256 "f0c1f9052eeda25ca4f86aa343fb009a0289f153689765e45dbd17272880cde8"
+  version "1.2"
+  sha256 "9be80c04ccdb4c4d78943356ba964e34ebef54794814f89d61d4fdd18e1c1869"
 
   url "https://github.com/kageroumado/rocuronium/releases/download/v#{version}/Rocuronium-#{version}.dmg"
   name "Rocuronium"
